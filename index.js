76295f190cb2b371ef8afe94602b52db9aee0bee
Ritam2000RK <rtmkayal@gmail.com>
2023-09-02 04:56:44 +0530

    added some css files

---
+++ index.js
@@ -0,0 +1,7 @@
+// const contact = document.getElementById("footer-content");
+
+// contact.style.display = 'none';
+
+// contact.onclick = function () {
+//     const cont = document.getElementById("footer-content");
+// }
